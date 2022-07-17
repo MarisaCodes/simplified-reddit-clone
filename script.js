460dd2eb0d4d945b69b44a00d13a853e167ab873
MarisaCodes <marisakirisame955@gmail.com>
2022-07-17 10:08:35 +0400

    deleted lines from script.js, instead added proper media queries to style.css

--- script.js
+++
@@ -1,8 +0,0 @@
-const pen = document.querySelector(".pen-icon");
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 768) {
-    pen.style.setProperty("display", "none");
-  } else {
-    pen.style.setProperty("display", "inline");
-  }
-});
