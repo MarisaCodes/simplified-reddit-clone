50819375b8e5e93a110a269000f8f5c5c6e7288c
MarisaCodes <marisakirisame955@gmail.com>
2022-07-18 08:56:30 +0400

    cleaned things up, added more comments and added input validation to script file

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-const createPost = document.querySelector(".create-post-wrapper");
-const community = document.querySelector(".communities");
-const communityUl = document.querySelector(".community-ul");
+if (window.location.pathname === "/create.html") {
+  const community = document.querySelector(".communities");
+  //clicking the downwards arrow in the create.html page
+  document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("down-arrow")) {
+      if (community.classList.contains("display")) {
+        community.classList.remove("display");
+      } else {
+        community.classList.add("display");
+      }
+    } //clicking the arrow will either show the dropdown list or hide it
 
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("down-arrow")) {
-    if (community.classList.contains("display")) {
-      community.classList.remove("display");
-    } else {
-      community.classList.add("display");
-    }
-  }
-
-  const clickInsidecommunityUl = communityUl.contains(e.target);
-  if (!e.target.classList.contains("down-arrow")) {
-    if (!e.target.classList.contains("community-input")) {
-      if (!clickInsidecommunityUl) {
-        if (!community.classList.contains("display")) {
-          community.classList.add("display");
+    const clickInsidecommunity = community.contains(e.target);
+    if (!e.target.classList.contains("down-arrow")) {
+      //clicking outside to hide dropdown menu
+      if (!e.target.classList.contains("community-input")) {
+        if (!clickInsidecommunity) {
+          if (!community.classList.contains("display")) {
+            community.classList.add("display");
+          }
         }
       }
     }
+  });
+
+  const selectCommunity = document.querySelector(".community-input");
+  const title = document.querySelector("#user-title-input");
+  // const postText = document.querySelector("#user-textrea-input");
+  const postBtn = document.querySelector(".post-btn");
+
+  // first removing link to new page from a tag that surrounds button
+
+  postBtn.parentElement.removeAttribute("href");
+  function checkUserInput() {
+    /*this function checks whether the title input 
+    and the choose a community input are blank or not*/
+    if (title.value !== "" && selectCommunity.value !== "") {
+      postBtn.classList.remove("empty-input");
+
+      // now the user can go to post details page after the input is filled
+
+      postBtn.parentElement.setAttribute("href", "post-detail.html");
+      postBtn.style.cursor = "pointer";
+    } else {
+      // if they're blank, style the 'post' button this way
+      postBtn.classList.add("empty-input");
+      postBtn.parentElement.removeAttribute("href");
+      postBtn.style.cursor = "not-allowed";
+    }
   }
-});
+}
